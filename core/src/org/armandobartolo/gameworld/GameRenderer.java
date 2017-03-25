0bbd2c09de40efd711bb74d132fdf65116bb0449
armandobartolo <armando.parreira.bartolo@gmail.com>
2017-03-25 22:36:52 +0000

    added pipes and grass scrolling

--- core/src/org/armandobartolo/gameworld/GameRenderer.java
+++ core/src/org/armandobartolo/gameworld/GameRenderer.java
@@ -10,6 +10,9 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import org.armandobartolo.gameobjects.Bird;
+import org.armandobartolo.gameobjects.Grass;
+import org.armandobartolo.gameobjects.Pipe;
+import org.armandobartolo.gameobjects.ScrollHandler;
 import org.armandobartolo.zbhelpers.AssetLoader;
 
 
@@ -28,6 +31,9 @@ public class GameRenderer {
     private int gameHeight;
 
     private Bird bird;
+    private Grass frontGrass, backGrass;
+    private Pipe pipe1, pipe2, pipe3;
+    private ScrollHandler scroller;
 
     private TextureRegion bg, grass;
     private Animation birdAnimation;
@@ -55,6 +61,13 @@ public class GameRenderer {
 
     private void initGameObjects() {
         bird = world.getBird();
+        scroller = world.getScroller();
+        frontGrass = scroller.getFrontGrass();
+        backGrass = scroller.getBackGrass();
+        pipe1 = scroller.getPipe1();
+        pipe2 = scroller.getPipe2();
+        pipe3 = scroller.getPipe3();
+
     }
 
     private void initAssets() {
@@ -103,9 +116,9 @@ public class GameRenderer {
         // The bird needs transparency, so we enable that again.
         batcher.enableBlending();
 
-        // Draw bird at its coordinates. Retrieve the Animation object from
-        // AssetLoader
-        // Pass in the runTime variable to get the current frame.
+        drawGrass();
+        drawPipes();
+        drawSkulls();
 
         if (bird.shouldntFlap()) {
             batcher.draw(birdMid,
@@ -122,4 +135,52 @@ public class GameRenderer {
         // End SpriteBatch
         batcher.end();
     }
+
+    private void drawGrass() {
+        // Draw the grass
+        batcher.draw(grass, frontGrass.getX(), frontGrass.getY(),
+                frontGrass.getWidth(), frontGrass.getHeight());
+        batcher.draw(grass, backGrass.getX(), backGrass.getY(),
+                backGrass.getWidth(), backGrass.getHeight());
+    }
+
+    private void drawSkulls() {
+        // Temporary code! Sorry about the mess :)
+        // We will fix this when we finish the Pipe class.
+
+        batcher.draw(skullUp, pipe1.getX() - 1,
+                pipe1.getY() + pipe1.getHeight() - 14, 24, 14);
+        batcher.draw(skullDown, pipe1.getX() - 1,
+                pipe1.getY() + pipe1.getHeight() + 45, 24, 14);
+
+        batcher.draw(skullUp, pipe2.getX() - 1,
+                pipe2.getY() + pipe2.getHeight() - 14, 24, 14);
+        batcher.draw(skullDown, pipe2.getX() - 1,
+                pipe2.getY() + pipe2.getHeight() + 45, 24, 14);
+
+        batcher.draw(skullUp, pipe3.getX() - 1,
+                pipe3.getY() + pipe3.getHeight() - 14, 24, 14);
+        batcher.draw(skullDown, pipe3.getX() - 1,
+                pipe3.getY() + pipe3.getHeight() + 45, 24, 14);
+    }
+
+    private void drawPipes() {
+        // Temporary code! Sorry about the mess :)
+        // We will fix this when we finish the Pipe class.
+        batcher.draw(bar, pipe1.getX(), pipe1.getY(), pipe1.getWidth(),
+                pipe1.getHeight());
+        batcher.draw(bar, pipe1.getX(), pipe1.getY() + pipe1.getHeight() + 45,
+                pipe1.getWidth(), midPointY + 66 - (pipe1.getHeight() + 45));
+
+        batcher.draw(bar, pipe2.getX(), pipe2.getY(), pipe2.getWidth(),
+                pipe2.getHeight());
+        batcher.draw(bar, pipe2.getX(), pipe2.getY() + pipe2.getHeight() + 45,
+                pipe2.getWidth(), midPointY + 66 - (pipe2.getHeight() + 45));
+
+        batcher.draw(bar, pipe3.getX(), pipe3.getY(), pipe3.getWidth(),
+                pipe3.getHeight());
+        batcher.draw(bar, pipe3.getX(), pipe3.getY() + pipe3.getHeight() + 45,
+                pipe3.getWidth(), midPointY + 66 - (pipe3.getHeight() + 45));
+    }
+
 }
--- core/src/org/armandobartolo/gameworld/GameWorld.java
+++ core/src/org/armandobartolo/gameworld/GameWorld.java
@@ -1,8 +1,9 @@
 package org.armandobartolo.gameworld;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.math.Rectangle;
 import org.armandobartolo.gameobjects.Bird;
+import org.armandobartolo.gameobjects.Grass;
+import org.armandobartolo.gameobjects.Pipe;
+import org.armandobartolo.gameobjects.ScrollHandler;
 
 /**
  * Created by armando on 25-03-2017.
@@ -10,18 +11,28 @@ import org.armandobartolo.gameobjects.Bird;
 public class GameWorld {
 
     private Bird bird;
+    private ScrollHandler scroller;
 
     public GameWorld(int midPointY) {
         bird = new Bird(33, midPointY - 5, 17, 12);
+        scroller = new ScrollHandler(midPointY + 66);
+
     }
 
     public void update(float delta) {
 
         bird.update(delta);
+        scroller.update(delta);
 
     }
+
 
     public Bird getBird() {
         return bird;
     }
+
+    public ScrollHandler getScroller() {
+        return scroller;
+    }
+
 }
