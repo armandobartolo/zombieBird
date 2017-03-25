67b64599925cb98fffb7be7df5858a56479874dd
Armando <armando.parreira.bartolo@gmail.com>
2017-03-25 16:30:37 +0000

    started again

---
+++ desktop/src/org/armandobartolo/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.armandobartolo.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.armandobartolo.ZBGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ZBGame(), config);
+	}
+}
